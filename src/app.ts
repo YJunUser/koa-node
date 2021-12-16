6b0207bef67c5d552401f753d4a2c7f92b0cd789
姚伯骏 <yaobojun@yaobojundeMacBook-Air.local>
2021-12-16 20:10:24 +0800

    添加编译脚本

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@ import * as Koa from 'koa';
 import * as BodyParser from 'koa-bodyparser';
 import catchError from './middleware/catch';
 import validate from './middleware/validate';
-import Routing from './routes';
+import router from './routes';
 const app = new Koa();
 
 app.use(BodyParser());
 app.use(validate);
 app.use(catchError);
-Routing(app);
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, 'localhost', () => {
   console.log('server is running at 3000');
